65c63cab7d330a52da8ee50a5bc90a0f027e3e3d
Ricardo Oliveira <ricardolotr@gmail.com>
2013-05-17 20:56:38 +0100

    additional tests

--- src/Ricoli/Nearest/Nearest.php
+++ src/Ricoli/Nearest/Nearest.php
@@ -22,6 +22,9 @@ class Nearest
             throw new \InvalidArgumentException(sprintf('Invalid timestamp provided: "%s". It must be a greater than 0 integer.', $timestamp));
         }
 
+        if (!in_array($mode, array(PHP_ROUND_HALF_UP, PHP_ROUND_HALF_DOWN, PHP_ROUND_HALF_EVEN, PHP_ROUND_HALF_ODD))) {
+            throw new \InvalidArgumentException(sprintf('Invalid mode provided: "%s". It must be a mode supported by the round function.', $mode));
+        }
         return (int) round($timestamp/$seconds, 0, $mode)*$seconds;
     }
 }
--- src/Ricoli/Nearest/Tests/NearestTest.php
+++ src/Ricoli/Nearest/Tests/NearestTest.php
@@ -48,9 +48,41 @@ class NearestTest extends \PHPUnit_Framework_TestCase
             array(1.1)
         );
     }
+    
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function test_getNearestTimestamp_throws_InvalidArgumentException_if_invalid_mode_is_passed()
+    {
+        $this->nearest->getNearestTimestamp(1, 1, 'this is sparta!');
+    }
+    
+    public function test_getNearestTimestamp_returns_nearest_timestamp_according_to_number_of_seconds()
+    {
+        $this->assertSame(915195600, $this->nearest->getNearestTimestamp(3600, 915195750));
+        $this->assertSame(915235200, $this->nearest->getNearestTimestamp(86400, 915195750));
+    }
 
     public function test_getNearestTimestamp_returns_nearest_timestamp_using_default_mode()
     {
         $this->assertSame(1368801000, $this->nearest->getNearestTimestamp(300, 1368801112));
+    }
+    
+    /**
+     * @dataProvider getModesTestCases
+     */
+    public function test_getNearestTimestamp_returns_nearest_timestamp_using_according_to_mode($mode, $expectedTimestamp)
+    {
+        $this->assertSame($expectedTimestamp, $this->nearest->getNearestTimestamp(300, 915195750, $mode));
+    }
+    
+    public function getModesTestCases()
+    {
+        return array(
+            array(PHP_ROUND_HALF_DOWN, 915195600),
+            array(PHP_ROUND_HALF_UP, 915195900),
+            array(PHP_ROUND_HALF_ODD, 915195900),
+            array(PHP_ROUND_HALF_EVEN, 915195600),
+        );
     }
 }
